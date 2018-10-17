5365a150ea6b5bf1f122f847dbd13b342de24980
Arief Hidayatulloh <ariefhidayatulloh@gmail.com>
2018-10-17 07:14:51 +0700

    remove unnecessary

--- set_admin.go
+++
@@ -1,5 +0,0 @@
-package event
-
-type SetAdminPayload struct {
-	ID string `json:"id"`
-}
--- user_created.go
+++
@@ -1,8 +0,0 @@
-package event
-
-type UserCreatedPayload struct {
-	ID           string `json:"id"`
-	Username     string `json:"username"`
-	Name         string `json:"name"`
-	HashPassword string `json:"hash_password"`
-}
